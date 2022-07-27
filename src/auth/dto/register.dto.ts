a399ce82103df522d2ebe9e7381214d13aa3298b
Mohammad Reza <reza.najibf@gmail.com>
2022-07-28 01:07:23 +0430

    feat: auth initial implementation

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
